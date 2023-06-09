f93b71f05a8663be04d828aeb1a23026cc3970b5
L P <xpacako1@mendelu.cz>
2023-06-09 20:55:57 +0200

    model transaction

---
+++ app/src/main/java/cz/mendelu/pef/dostihyasazky/model/Transaction.kt
@@ -0,0 +1,17 @@
+package cz.mendelu.pef.dostihyasazky.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Transaction(
+    @ColumnInfo(name = "event")
+    var event: String,
+
+    @ColumnInfo(name = "value")
+    var value: Int
+){
+    @PrimaryKey(autoGenerate = true)
+    var id: Long? = null
+}
